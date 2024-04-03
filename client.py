36125f8f7afce013fc586f5c21f0e03513461efc
Vishal <96231081+vishuuwu@users.noreply.github.com>
2024-04-03 10:50:58 +0530

    Made it a actual multiplayer game 😎
    Capable of managing multiple players (clients) simultaneously.
    Implements dynamic allocation of IP addresses to the server (hint: check the comments for my IP address 😉,  tone : sarcastic, pfft).
    
    Initially attempted to finally use sprites instead of simple rectangles. However, encountered an issue with pygame.surface, which cannot be serialized using pickle (to the best of my knowledge at the time of writing this message). Possible solutions include
    -  finding a method to serialize pygame.surface using pickle, 
    - avoiding the use of pickle altogether, 
    - or creating player objects on the client side (not recommended, potential burden of each client having to create every player in the lobby - bad idea, but only possible way as of now).

--- client.py
+++ client.py
@@ -1,22 +1,79 @@
 import pygame
 from network import Network
-from player import Player
-
-# Constants
-WIDTH = 1000
-HEIGHT = 800
-FPS = 60
-
-# Player constants
-PLAYER_WIDTH = 100
-PLAYER_HEIGHT = 100
-PLAYER_COLOR = (0, 0, 255)
-PLAYER_TWO_COLOR = (0, 255, 0)
+# from player import Player
+from game_config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+from os import listdir
+from os.path import join, isfile
 
 
+def flip_sprite(sprite):
+    return pygame.transform.flip(sprite, True, False)
+
+def load_character_sprites(direction=False):
+    dir = "characters"
+    path = join("assets", dir)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_character_sprites = {}
+
+    for image in images:
+        sprite = pygame.image.load(join(path, image)).convert_alpha()
+
+        if direction:
+            all_character_sprites[image.replace(".png", "") + "_right"] = sprite
+            all_character_sprites[image.replace(".png", "") + "_left"] = flip_sprite(sprite)
+        else:
+            all_character_sprites[image.replace(".png", "")] = sprite
+    return all_character_sprites
 
 
-def redraw_window(win, player, player2):
+#--------------------------------------------------------
+"""
+TypeError: cannot pickle 'pygame.surface.Surface' object` 
+
+pickle cant serialize (or i am not able to serialize..) pygame.surface object
+"""
+
+# def draw(player, win, SPRITES):
+#     """
+#         Draw the player on the window.
+
+#         Args:
+#             win (pygame.Surface): The window surface to draw the player on.
+#     """
+#     player.sprite = SPRITES[player.char + "_" + player.direction]
+#     win.blit(player.sprite, (player.x, player.y))
+
+# def redraw_window(win, player, opponents):
+#     """
+#     Redraw the window with a white background and the player.
+
+#     Args:
+#         win (pygame.Surface): The window surface to draw on.
+#         player (Player): The player object to draw.
+#     """
+#     win.fill((255, 255, 255))
+#     draw(player,win, SPRITES)
+#     for opp_id, opp in opponents.items():
+#         draw(opp,win, SPRITES)
+
+#     pygame.display.update()
+
+#--------------------------------------------------------
+
+
+#--------------------------------------------------------
+
+def draw(player, win):
+    """
+    Draw the player on the window.
+
+        Args:
+        win (pygame.Surface): The window surface to draw the player on.
+    """
+    pygame.draw.rect(win, player.color, player.rect)
+
+def redraw_window(win, player, opponents):
     """
     Redraw the window with a white background and the player.
 
@@ -25,10 +82,14 @@ def redraw_window(win, player, player2):
         player (Player): The player object to draw.
     """
     win.fill((255, 255, 255))
-    player.draw(win)
-    player2.draw(win)
+    draw(player,win)
+    for opp_id, opp in opponents.items():
+        draw(opp,win)
+
     pygame.display.update()
 
+
+#--------------------------------------------------------
 
 def handle_events():
     """Handle events such as quitting the game."""
@@ -37,32 +98,33 @@ def handle_events():
             return False
     return True
 
-
-
 def main():
     """Main function to run the game."""
-    pygame.init()
-    win = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Client")
-
     running = True
     n = Network()
     player =n.getPlayer()
-
-  
+    print(player)
+    print(player.char)
     clock = pygame.time.Clock()
     while running:
         clock.tick(FPS)
 
-        p2 = n.send(player)
-
+        opponents = n.send(player)
+        # print(len(opponents))
         running = handle_events()
+        if running == False :
+            print("Disconnected")
+            n.disconnect()
 
         player.move()
-        redraw_window(win, player, p2)
+        redraw_window(win, player, opponents)
 
     pygame.quit()
 
 
 if __name__ == "__main__":
+    pygame.init()
+    win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Client")
+    SPRITES = load_character_sprites(True)
     main()
